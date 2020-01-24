9846e30027d305e203cc30e5ee82d7d2b1d148d4
Alisson Youssf <youssfbr@gmail.com>
2020-01-24 20:12:10 -0300

    Corrigindo: Adicionando Métodos Setters

--- src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
+++ src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
@@ -4,12 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alissondev.comercial.model.Oportunidade;
+
 @RestController
 @RequestMapping("/oportunidades")
 public class OportunidadeController {
 	
 	@GetMapping
 	public String listar() {
+		Oportunidade oportunidade = new Oportunidade();
+		oportunidade.
 		return "Hello";
 	}
 
