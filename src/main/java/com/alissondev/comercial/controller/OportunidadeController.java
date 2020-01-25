c41beca9fe36cf0a92d177223da7f00de453f959
Alisson Youssf <youssfbr@gmail.com>
2020-01-25 08:38:30 -0300

    Retornando dados do Banco de Dados

--- src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
+++ src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
@@ -1,28 +1,25 @@
 package com.alissondev.comercial.controller;
 
-import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alissondev.comercial.model.Oportunidade;
+import com.alissondev.comercial.repository.OportunidadeRepository;
 
 @RestController
 @RequestMapping("/oportunidades")
 public class OportunidadeController {
 	
+	@Autowired
+	private OportunidadeRepository oportunidades;
+	
 	@GetMapping
 	public List<Oportunidade> listar() {
-		Oportunidade oportunidade = new Oportunidade();
-		oportunidade.setId(1L);
-		oportunidade.setDescricao("Desenvolvimento de ERP com Angular e Spring");
-		oportunidade.setNomeProspecto("Grupo Logística Brasil 222");
-		oportunidade.setValor(new BigDecimal(500000));
-		
-		return Arrays.asList(oportunidade);
+		return oportunidades.findAll();
 	}
 
 }
