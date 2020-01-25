e943c879aba18ea7b9bb6b6a033a8b87fa8da63c
Alisson Youssf <youssfbr@gmail.com>
2020-01-25 09:22:21 -0300

    Retornando dados do Banco de Dados pelo Id

--- src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
+++ src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
@@ -1,9 +1,12 @@
 package com.alissondev.comercial.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,16 @@ public class OportunidadeController {
 	@GetMapping
 	public List<Oportunidade> listar() {
 		return oportunidades.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Oportunidade> buscar(@PathVariable Long id) {
+		Optional<Oportunidade> oportunidade = oportunidades.findById(id);
+		
+		if (oportunidade.isEmpty()) 
+			return ResponseEntity.notFound().build();		
+		
+		return ResponseEntity.ok(oportunidade.get());
 	}
 
 }
