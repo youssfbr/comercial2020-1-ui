1fee7cfb96ff0015726d136dac54653da90563ac
Alisson Youssf <youssfbr@gmail.com>
2020-01-28 16:00:52 -0300

    Implementando método salvar

--- src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
+++ src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alissondev.comercial.model.Oportunidade;
@@ -34,5 +38,9 @@ public class OportunidadeController {
 		
 		return ResponseEntity.ok(oportunidade.get());
 	}
-
+	
+	@PostMapping	
+	public Oportunidade adicionar(@RequestBody Oportunidade oportunidade) {
+		return oportunidades.save(oportunidade);
+	}
 }
