47819b5d9537c2eac77da4542fa4c96e2a2d96ab
Alisson Youssf <youssfbr@gmail.com>
2020-01-28 16:28:18 -0300

    Criado método que busca pela descrição e pelo nomeProspecto

--- src/main/java/com/alissondev/comercial/repository/OportunidadeRepository.java
+++ src/main/java/com/alissondev/comercial/repository/OportunidadeRepository.java
@@ -1,9 +1,13 @@
 package com.alissondev.comercial.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.alissondev.comercial.model.Oportunidade;
 
 public interface OportunidadeRepository extends JpaRepository<Oportunidade, Long> {
+	
+	Optional<Oportunidade> findByDescricaoAndNomeProspecto(String descricao, String nomeProspecto);
 
 }
