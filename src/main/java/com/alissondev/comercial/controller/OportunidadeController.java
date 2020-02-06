e94ba6b0e63872b6e321f1176a0739cd241ed85e
Alisson Youssf <youssfbr@gmail.com>
2020-02-06 19:13:27 -0300

    Implementando método de atualização

--- src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
+++ src/main/java/com/alissondev/comercial/controller/OportunidadeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -64,5 +65,11 @@ public class OportunidadeController {
 	@DeleteMapping("/{id}")
 	public void deletar(@PathVariable Long id) {
 		oportunidades.deleteById(id); 
+	}
+	
+	@PutMapping("/{id}")
+	public void atualizar(@RequestBody Oportunidade oportunidade, @PathVariable Long id) {
+		oportunidade.setId(id);
+		oportunidades.save(oportunidade);
 	}
 }
